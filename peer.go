5a8828fea21587c3c501f9ca72b239887486692d
Benny Daon <benny@tuzig.com>
2021-03-30 17:05:48 +0300

    Improving ping period

--- peer.go
+++ peer.go
@@ -22,7 +22,7 @@ const (
 	pongWait = 5 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
+	pingPeriod = 30 * time.Second
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
