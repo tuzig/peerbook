fcda376ae932dfef1e53f9fac5f8059960de00d9
Benny Daon <benny@tuzig.com>
2021-03-30 23:10:35 +0300

    Allowing browsers to connect

--- peer.go
+++ peer.go
@@ -36,6 +36,7 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 // Peer is a middleman between the websocket connection and the hub.
