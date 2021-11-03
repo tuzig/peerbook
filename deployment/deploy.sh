f9bf412cec62545aa19cb943ab59b2ec0ba669b0
Amit Shalev <amit30005@gmail.com>
2021-11-03 19:35:02 +0200

    build and push docker, docker compose ,ecs params

---
+++ deployment/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+VERSION="$(git rev-parse --short HEAD)"
+CLUSTER=""
+TARGET_GROUP_ID=""
+
+echo "Preparing docker compose"
+sed -e "s/\${AWS_REGION}/${AWS_REGION}/g; s/\${AWS_ACCOUNT_ID}/${AWS_ACCOUNT_ID}/g; s/\${VERSION}/${VERSION}/g" docker-compose.template.yml > docker-compose.yml
+
+echo "Preparing ecs params"
+sed -e "s/\${AWS_REGION}/${AWS_REGION}/g; s/\${AWS_ACCOUNT_ID}/${AWS_ACCOUNT_ID}/g" ecs-params.template.yml > ecs-params.yml
+
+ecs-cli compose --project-name tools \
+  service up \
+  --create-log-groups \
+  --cluster-config "${CLUSTER}" \
+  --cluster "${CLUSTER}" \
+  --target-groups "targetGroupArn=arn:aws:elasticloadbalancing:${AWS_REGION}:${AWS_ACCOUNT_ID}:targetgroup/${TARGET_GROUP_ID},containerName=api,containerPort=17777" \
+  --region "${AWS_REGION}" \
+  --deployment-min-healthy-percent 100 \
+  --deployment-max-percent 200 \
+  --timeout 5
+
+if [ "$?" = "1" ]; then
+  echo "Deploy failed"
+  exit 1
+fi
