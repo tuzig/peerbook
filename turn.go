c5f96d5ea6a1b3e84c7a54dc88a30e06e238ff20
Benny Daon <benny@tuzig.com>
2023-01-10 10:40:06 +0200

    REmoving unused subspace code

--- turn.go
+++ turn.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-	"time"
 )
 
 const credentialsURL = "https://api.subspace.com/v1/globalturn"
@@ -50,56 +47,4 @@ func serveTURN(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	w.Write([]byte("]"))
-}
-
-func getSubspaceToken() (string, error) {
-	subspaceId := os.Getenv("SUBSPACE_ID")
-	if subspaceId == "" {
-		return "", fmt.Errorf("SUBSPACE_ID is not set")
-	}
-	msg := map[string]string{
-		"client_id":     subspaceId,
-		"client_secret": os.Getenv("SUBSPACE_SECRET"),
-		"audience":      "https://api.subspace.com/",
-		"grant_type":    "client_credentials"}
-	m, err := json.Marshal(msg)
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	resp, err := client.Post(tokenURL, "application/json", bytes.NewBuffer(m))
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("Got error %d from subspace", resp.StatusCode)
-	}
-	var ret map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&ret)
-	if err != nil {
-		Logger.Warnf("Failed to get subspace token: %s", err)
-		http.Error(w, fmt.Sprintf("Failed to get token: %s", err),
-			http.StatusInternalServerError)
-		return
-	}
-	t = ret["access_token"].(string)
-	// TODO cache the bearerAuth using redis
-	bearerAuth := fmt.Sprintf("Bearer %s", t)
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	req, err := http.NewRequest("POST", credentialsURL, nil)
-	if err != nil {
-		return "", fmt.Errorf("Failed to get redetials from subspace: %s", err)
-	}
-	req.Header.Add("authorization", bearerAuth)
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("Failed to get turn server")
-	}
-	if resp.StatusCode == http.StatusUnauthorized {
-		Logger.Warn("Our TURN credentials are outdates")
-		return "", fmt.Errorf("Uauthorized on the TURN service")
-	}
-	return bearerAuth, nil
 }
