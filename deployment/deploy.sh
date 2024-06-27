52ef5082365eaabef7b98dcafcf81378290dbae4
Benny Daon <benny@tuzig.com>
2024-06-27 15:44:50 +0300

    Fixing staging deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -24,8 +24,8 @@ if [ "${ENVIRONMENT}" = "staging" ]; then
                   aws s3 cp s3://${BUCKET_NAME}/nginx.conf .; \
                   aws s3 cp s3://${BUCKET_NAME}/docker-compose.yml .; \
                   aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.eu-central-1.amazonaws.com; \
-                  docker compose down; \
-                  docker compose up -d"
+                  docker-compose down; \
+                  docker-compose up -d"
 
     # Execute commands via SSM
     aws ssm send-command \
